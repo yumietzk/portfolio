144d5bef967aa8febbf9a28df57c8b85a4c378a7
yumietzk <yumie.t.29@gmail.com>
2021-09-10 18:04:26 +0900

    created about section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
-    title: 'Yumie.T | Front-end Develope',
+    title: 'Yumie.T | Front-end Developer',
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `assets`,
+        path: `${__dirname}/assets`,
+      },
+    },
+    'gatsby-plugin-mdx',
+  ],
 };
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import * as styles from './about.module.css';
 
 const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      mdx(slug: { eq: "aboutme" }) {
+        id
+        body
+      }
+    }
+  `);
+
   return (
-    <div className={styles.about}>
+    <section className={styles.about}>
       <h1>About</h1>
-    </div>
+      <p className={styles.aboutMe}>
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </p>
+      <h2>Skills + Tools</h2>
+    </section>
   );
 };
 
