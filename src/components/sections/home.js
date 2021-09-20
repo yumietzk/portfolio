44ee5892c1489e106ce2a529754c998779a95aa3
yumietzk <yumie.t.29@gmail.com>
2021-09-20 20:10:49 +0900

    implemented typewriter effect on home section

--- src/components/sections/home.js
+++ src/components/sections/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import scrollTo from 'gatsby-plugin-smoothscroll';
+import Typewriter from 'typewriter-effect';
 import * as IoIcons from 'react-icons/io';
 import Navigation from '../UI/navigation/navigation';
 import * as styles from './home.module.css';
@@ -25,7 +26,25 @@ const Home = () => {
       <div className={styles.headerTitle}>
         <div className={styles.headerText}>
           <h1>Hi, I'm Yumie 👋🏻</h1>
-          <h4>A front-end developer based in Toronto</h4>
+          {/* <h4>A front-end developer based in Toronto.</h4> */}
+          <Typewriter
+            options={{
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('A front-end developer based in Toronto.')
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString('A sushi lover from Japan.')
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString('A passionate football fan.')
+                .pauseFor(2000)
+                .deleteAll()
+                .start();
+            }}
+          />
         </div>
         <div className={styles.headerImg}>
           <div className={styles.headerPoster}>
