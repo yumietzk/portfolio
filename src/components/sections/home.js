e39071b5e1377cef414a017a30c28f0bd7a97fbb
yumietzk <yumie.t.29@gmail.com>
2021-09-27 16:02:09 -0400

    changed design on home section

--- src/components/sections/home.js
+++ src/components/sections/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+// import { useStaticQuery, graphql } from 'gatsby';
+// import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+// import { StaticImage } from 'gatsby-plugin-image';
 import scrollTo from 'gatsby-plugin-smoothscroll';
 import Typewriter from 'typewriter-effect';
 import * as IoIcons from 'react-icons/io';
@@ -8,49 +9,53 @@ import Navigation from '../UI/navigation/navigation';
 import * as styles from './home.module.css';
 
 const Home = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "home-image.png" }) {
-        childImageSharp {
-          gatsbyImageData
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     file(relativePath: { eq: "home-image.png" }) {
+  //       childImageSharp {
+  //         gatsbyImageData
+  //       }
+  //     }
+  //   }
+  // `);
 
-  const image = getImage(data.file);
+  // const image = getImage(data.file);
 
   return (
     <header className={styles.header} id="top">
       <Navigation />
       <div className={styles.headerTitle}>
-        <div className={styles.headerText}>
-          <h1>Hi, I'm Yumie 👋🏻</h1>
-          {/* <h4>A front-end developer based in Toronto.</h4> */}
-          <Typewriter
-            options={{
-              loop: true,
-            }}
-            onInit={(typewriter) => {
-              typewriter
-                .typeString('A front-end developer based in Toronto.')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('A sushi lover from Japan.')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('A passionate football fan.')
-                .pauseFor(2000)
-                .deleteAll()
-                .start();
-            }}
-          />
-        </div>
         <div className={styles.headerImg}>
-          <div className={styles.headerPoster}>
-            <GatsbyImage image={image} alt="My profile photo" />
+          <div className={styles.headerText}>
+            <h1>Hi, I'm Yumie 👋🏻</h1>
+            {/* <h4>A front-end developer based in Toronto.</h4> */}
+            <Typewriter
+              options={{
+                loop: true,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('A front-end developer based in Toronto.')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('A sushi lover from Japan.')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('A passionate football fan.')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .start();
+              }}
+            />
           </div>
         </div>
+        {/* <div className={styles.headerImg}>
+          <StaticImage
+            src="../../../assets/home-image.png"
+            alt="My profile photo"
+            className={styles.headerPoster}
+          />
+        </div> */}
       </div>
       <button className={styles.btn} onClick={() => scrollTo('#about')}>
         <IoIcons.IoIosArrowDropdownCircle className={styles.btnIcon} />
